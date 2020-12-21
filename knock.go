c8adc7ec583ac0b433b4c0543305319006916416
cmj <cmj@cmj.tw>
2020-12-21 21:50:42 +0800

    [WIP][Scan] receive by pcap.Handle and list all scan IP-list

--- knock.go
+++ knock.go
@@ -2,10 +2,8 @@ package knock
 
 import (
 	"fmt"
-	"net"
 	"os"
-	"strconv"
-	"strings"
+	"runtime"
 
 	"github.com/cmj0121/argparse"
 	"github.com/cmj0121/logger"
@@ -19,17 +17,22 @@ type Knock struct {
 	// the internal logger
 	*logger.Logger `-`
 	LogLevel       string `name:"log" choices:"warn info debug verbose" help:"log level"`
-	IFace          net.Interface
 
-	Port string  `short:"s" help:"port (INT), port list  (INT,INT) or port range (INT-INT)"`
-	IP   *string `help:"ip address (STR), ip list (STR,STR) or ip with mask (STR/INT)"`
+	// number of worker, default #CPU
+	NumWorker int `short:"w" name:"worker" help:"number of worker"`
 
 	*Info `help:"show self net info"`
+	*Scan `help:"run scan"`
 }
 
 func New() (knock *Knock) {
 	knock = &Knock{
-		Logger: logger.New(PROJ_NAME),
+		Logger:    logger.New(PROJ_NAME),
+		NumWorker: runtime.NumCPU(),
+		Scan: &Scan{
+			MaxPkgSize: MAX_PACKAGE_SIZE,
+			Timeout:    60,
+		},
 	}
 	return
 }
@@ -52,8 +55,6 @@ func (knock *Knock) ParseAndRun() {
 	knock.Logger.SetLevel(knock.LogLevel)
 	knock.Logger.Info("start run %v", PROJ_NAME)
 
-	knock.MustValidator()
-
 	switch {
 	case knock.Info != nil:
 		knock.Info.Load()
@@ -64,63 +65,10 @@ func (knock *Knock) ParseAndRun() {
 			// show on the STDOUT
 			os.Stdout.Write(data)
 		}
+	case knock.Scan != nil:
+		// run scan
+		knock.Scan.Run(knock.Logger)
 	}
-
-	return
-}
-
-func (knock *Knock) MustValidator() (ports []int) {
-	switch {
-	case knock.Port == "":
-		// not set the port number
-	case RE_PORT_LIST.MatchString(knock.Port):
-		for _, p := range strings.Split(knock.Port, ",") {
-			if port, err := strconv.Atoi(p); err != nil {
-				err = fmt.Errorf("invalid port %v: %v", p, err)
-				panic(err)
-			} else {
-				switch {
-				case port < 0:
-					err = fmt.Errorf("invalid port %v", port)
-					panic(err)
-				case port > 65535:
-					err = fmt.Errorf("invalid port %v", port)
-					panic(err)
-				}
-				// save to the port-list
-				ports = append(ports, port)
-			}
-		}
-	case RE_PORT_RANGE.MatchString(knock.Port):
-		port_range := strings.Split(knock.Port, "-")
-		if port_start, err := strconv.Atoi(port_range[0]); err != nil {
-			err = fmt.Errorf("invalid port %v: %v", port_start, err)
-			panic(err)
-		} else if port_end, err := strconv.Atoi(port_range[1]); err != nil {
-			err = fmt.Errorf("invalid port %v: %v", port_end, err)
-			panic(err)
-		} else {
-			switch {
-			case port_start < 0:
-				err = fmt.Errorf("invalid port start %v", port_start)
-				panic(err)
-			case port_end > 65535:
-				err = fmt.Errorf("invalid port end %v", port_end)
-				panic(err)
-			case port_start >= port_end:
-				err = fmt.Errorf("invalid port range %v-%v", port_start, port_end)
-				panic(err)
-			}
-
-			for p := port_start; p <= port_end; p++ {
-				ports = append(ports, p)
-			}
-		}
-	default:
-		err := fmt.Errorf("invalid port: %v", knock.Port)
-		panic(err)
-	}
-	knock.Debug("scan port #%v ports", len(ports))
 
 	return
 }
