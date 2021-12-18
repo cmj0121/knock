2ca4d4d50149bc62d1ce43b5ae30929e1e9e0de1
cmj <cmj@cmj.tw>
2021-12-18 09:06:35 +0800

    feat(ver): add the version info
    	- call `Version` to get the version info
    	- define the bump policy of the version

---
+++ config.go
@@ -0,0 +1,21 @@
+package knock
+
+import (
+	"fmt"
+)
+
+// the meta of this projecet
+const (
+	// project name
+	PROJ_NAME = "knock"
+	// project version
+	MAJOR = 0	// the API version, bump when change interface
+	MINOR = 0	// bump when the new feature implemented
+	MACRO = 0	// bump when bug-fixed only
+)
+
+// return the version info
+func Version() (ver string) {
+	ver = fmt.Sprintf("%s (%d.%d.%d)", PROJ_NAME, MACRO, MINOR, MACRO)
+	return
+}
