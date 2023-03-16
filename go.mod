9c1ec0809a4fb61135fe78ceb9224440fe3dc91b
cmj <cmj@cmj.tw>
2023-03-16 09:32:21 +0800

    feat(knock): setup the basic CLI
    	provides the basic CLI argument parser (via kong) and
    	the logger system (via zerolog)

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/cmj0121/knock
 
 go 1.20
+
+require (
+	github.com/alecthomas/kong v0.7.1
+	github.com/rs/zerolog v1.29.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- knock.go
+++ knock.go
@@ -1,7 +1,21 @@
 package knock
 
+import (
+	"os"
+
+	"github.com/alecthomas/kong"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
 // the knock instance to run the brute-force task
 type Knock struct {
+	// the command-line options
+	Debug bool `short:"d" help:"Show the debug message (auto apply --pretty-logger, -vvvv)."`
+
+	Quiet        bool `short:"q" group:"logger" xor:"verbose,quiet" help:"Disable all logger."`
+	Verbose      int  `short:"v" group:"logger" xor:"verbose,quiet" type:"counter" help:"Show the verbose logger."`
+	PrettyLogger bool `group:"logger" help:"Show the pretty logger."`
 }
 
 // create the Knock instance with the default settings.
@@ -13,6 +27,55 @@ func New() (knock *Knock) {
 // run the knock, parse by the passed arguments from CLI and return
 // the result.
 func (knock *Knock) Run() (exitcode int) {
+	kong.Parse(knock)
+
+	knock.prologue()
+	log.Info().Msg("start Knock ...")
+
 	exitcode = 0
+	return
+}
+
+// setup the necessary before run knock
+func (knock *Knock) prologue() {
+	knock.setupLogger()
+}
+
+// setup the logger sub-system
+func (knock *Knock) setupLogger() {
+	if knock.PrettyLogger {
+		writter := zerolog.ConsoleWriter{Out: os.Stderr}
+		log.Logger = zerolog.New(writter).With().Timestamp().Logger()
+	}
+
+	switch knock.Verbose {
+	case -1:
+		zerolog.SetGlobalLevel(zerolog.Disabled)
+	case 0:
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case 1:
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case 2:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case 3:
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	}
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+}
+
+// the callback function after kong parse the CLI arguments
+func (knock *Knock) AfterApply() (err error) {
+	if knock.Debug {
+		knock.PrettyLogger = true
+		knock.Verbose = 4
+	}
+
+	if knock.Quiet {
+		knock.Verbose = -1
+	}
+
 	return
 }
