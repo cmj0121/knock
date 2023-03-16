4d55fe3d2bdd7931874ac3d02bd4358cda57c8f3
cmj <cmj@cmj.tw>
2023-03-16 14:41:38 +0800

    test(worker): add the example for the worker/Debug

---
+++ task/worker/debug_test.go
@@ -0,0 +1,20 @@
+package worker
+
+import (
+	"github.com/cmj0121/knock/task/producer"
+)
+
+func ExampleDebug() {
+	producer, _ := producer.NewCIDRProducer("127.0.0.1/30")
+
+	debug := Debug{}
+	debug.Open()        // nolint
+	defer debug.Close() // nolint
+
+	debug.Run(producer.Produce()) // nolint
+	// Output:
+	// 127.0.0.0
+	// 127.0.0.1
+	// 127.0.0.2
+	// 127.0.0.3
+}
