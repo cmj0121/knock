e41a87a8542fdff6162ef1a824d0e5eb49a2a0b7
cmj <cmj@cmj.tw>
2023-04-13 10:20:19 +0800

    chore(worker|subp): make argument more robust
    	auto check the protocol of passed argument

--- task/worker/subpath.go
+++ task/worker/subpath.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/cmj0121/knock/progress"
 	"github.com/rs/zerolog/log"
@@ -29,73 +30,81 @@ func init() {
 // the debugger worker and just show the word in STDOUT
 type SubPath struct {
 	*http.Client
-	// the target hostname/IP
-	hostname string
+
+	url *url.URL
 }
 
 // the unique name of worker
-func (s SubPath) Name() string {
+func (ctx SubPath) Name() string {
 	return "subp"
 }
 
 // show the help message
-func (s SubPath) Help() string {
+func (ctx SubPath) Help() string {
 	return "list possible path"
 }
 
 // the dummy open method
-func (s *SubPath) Open(args ...string) (err error) {
+func (ctx *SubPath) Open(args ...string) (err error) {
 	// check the wildcard IP address
 	switch len(args) {
 	case 0:
-		err = fmt.Errorf("should pass hostname to the command %#v", s.Name())
+		err = fmt.Errorf("should pass hostname to the command %#v", ctx.Name())
 	case 1:
-		s.hostname = args[0]
+		if ctx.url, err = url.Parse(args[0]); err == nil {
+			switch ctx.url.Scheme {
+			case "http":
+			case "https":
+			default:
+				err = fmt.Errorf("should provides the protocol: %v", args[0])
+				return
+			}
+		}
 	default:
-		err = fmt.Errorf("should pass one and only one hostname to the command %#v", s.Name())
+		err = fmt.Errorf("should pass one and only one hostname to the command %#v", ctx.Name())
 	}
 	return
 }
 
 // the dummy close method
-func (s SubPath) Close() (err error) {
+func (ctx SubPath) Close() (err error) {
 	log.Debug().Msg("dummy close")
 	return
 }
 
 // execute the worker
-func (s *SubPath) Run(producer <-chan string) (err error) {
+func (ctx *SubPath) Run(producer <-chan string) (err error) {
 	for word := range producer {
 		log.Debug().Str("word", word).Msg("handle producer")
 		progress.AddProgress(word)
 
-		url := fmt.Sprintf("%v/%v", s.hostname, word)
-		s.check(url, http.MethodGet)
-		s.check(url, http.MethodPost)
-		s.check(url, http.MethodPut)
-		s.check(url, http.MethodDelete)
+		url := fmt.Sprintf("%v/%v", ctx.url, word)
+		ctx.check(url, http.MethodGet)
+		ctx.check(url, http.MethodPost)
+		ctx.check(url, http.MethodPut)
+		ctx.check(url, http.MethodDelete)
 	}
 	return
 }
 
 // copy the current worker settings and generate a new instance
-func (s *SubPath) Dup() (worker Worker) {
+func (ctx *SubPath) Dup() (worker Worker) {
 	worker = &SubPath{
-		Client:   s.Client,
-		hostname: s.hostname,
+		Client: ctx.Client,
+		url:    ctx.url,
 	}
 	return
 }
 
 // check the folder with multiple possible HTTP method
-func (s *SubPath) check(url, method string) (code int) {
+func (ctx *SubPath) check(url, method string) (code int) {
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		progress.AddError(err)
 		return
 	}
 
-	switch resp, err := s.Client.Do(req); err {
+	switch resp, err := ctx.Client.Do(req); err {
 	case nil:
 		switch code = resp.StatusCode; code {
 		case 404:
